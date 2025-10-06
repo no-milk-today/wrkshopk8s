6a61613a17f79ea46962724b915e673757b5c307
prista <games.prista@mail.ru>
2025-10-06 18:50:33 +0300

    #remove extra

--- exchange-service/src/main/java/com/example/exchange/ExchangeApplication.java
+++ exchange-service/src/main/java/com/example/exchange/ExchangeApplication.java
@@ -2,13 +2,8 @@ package com.example.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySources({
-        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
-})
 public class ExchangeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExchangeApplication.class, args);
