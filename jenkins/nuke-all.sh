5e980d5ba5d313402b5932537597155860ec2e80
prista <games.prista@mail.ru>
2025-10-08 15:43:21 +0300

    #update Jenkinsfile. Add isssuer_uri to customer-service

--- jenkins/nuke-all.sh
+++ jenkins/nuke-all.sh
@@ -16,6 +16,12 @@ echo "Using DOCKER_REGISTRY: $DOCKER_REGISTRY"
 
 echo "Uninstalling Helm releases..."
 for ns in test prod; do
+  helm uninstall keycloak -n "$ns" || true
+  helm uninstall front-ui-service -n "$ns" || true
+  helm uninstall fraud-service -n "$ns" || true
+  helm uninstall notification-service -n "$ns" || true
+  helm uninstall exchange-generator-service -n "$ns" || true
+  helm uninstall exchange-service -n "$ns" || true
   helm uninstall customer-service -n "$ns" || true
   helm uninstall order-service -n "$ns" || true
   helm uninstall postgres -n "$ns" || true
@@ -24,6 +30,9 @@ done
 echo "Deleting secrets..."
 for ns in test prod; do
   kubectl delete secret customer-service-customer-db -n "$ns" --ignore-not-found
+  kubectl delete secret fraud-service-fraud-db -n "$ns" --ignore-not-found
+  kubectl delete secret notification-service-notification-db -n "$ns" --ignore-not-found
+  kubectl delete secret exchange-service-exchange-db -n "$ns" --ignore-not-found
   kubectl delete secret order-service-order-db -n "$ns" --ignore-not-found
 done
 
@@ -38,6 +47,11 @@ docker volume rm jenkins_home || true
 
 echo "Removing images..."
 docker image rm customer-service:latest || true
+docker image rm fraud-service:latest || true
+docker image rm notification-service:latest || true
+docker image rm exchange-service:latest || true
+docker image rm exchange-generator-service:latest || true
+docker image rm front-ui-service:latest || true
 docker image rm order-service:latest || true
 docker image rm jenkins-jenkins:latest || true
 
