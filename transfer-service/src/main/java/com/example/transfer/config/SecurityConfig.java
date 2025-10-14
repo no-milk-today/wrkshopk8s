17f8d3a37c09843ed25d974a9cc3b957634a291e
prista <games.prista@mail.ru>
2025-10-14 13:25:32 +0300

    #enable actuator endpoints

--- transfer-service/src/main/java/com/example/transfer/config/SecurityConfig.java
+++ transfer-service/src/main/java/com/example/transfer/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth -> oauth
