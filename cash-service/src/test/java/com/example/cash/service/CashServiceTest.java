c62ae5a3da248c00ff620869a843376ea870e2d9
prista <games.prista@mail.ru>
2025-10-27 13:19:02 +0300

    #cash service test fix

--- cash-service/src/test/java/com/example/cash/service/CashServiceTest.java
+++ cash-service/src/test/java/com/example/cash/service/CashServiceTest.java
@@ -13,8 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -36,6 +38,10 @@ class CashServiceTest {
     @InjectMocks
     private CashService cashService;
 
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(cashService, "cashNotificationTopic", "cash-notification");
+    }
 
     private static CustomerDto buildCustomer(String login, BigDecimal rubBalance) {
         var rubAccount = new AccountDto(
