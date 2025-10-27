cd1fce503a7e192f35ee28532158d9fcf1b9433f
prista <games.prista@mail.ru>
2025-10-27 17:55:44 +0300

    #resolve test

--- exchange-generator-service/src/test/java/com/example/exchangegenerator/service/ExchangeGeneratorServiceIntegrationTest.java
+++ exchange-generator-service/src/test/java/com/example/exchangegenerator/service/ExchangeGeneratorServiceIntegrationTest.java
@@ -27,80 +27,73 @@ import java.util.stream.StreamSupport;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
-@EmbeddedKafka(topics = {"exchange-rates"}, partitions = 1)
-@TestPropertySource(properties = {
-    "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
-    "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
-    "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}",
-    "spring.profiles.active=default"
-})
-@ActiveProfiles("default")
+@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
+@EmbeddedKafka(partitions = 1, topics = {"exchange-rates"}, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @DirtiesContext
-@Disabled // works locally fine, but fails in Jenkins (I have no idea why)
-// todo: it says java.lang.NullPointerException: Cannot invoke "com.example.exchangegenerator.service.ExchangeGeneratorService.generateAndUpdateRates()" because "this.exchangeGeneratorService" is null
-// at com.example.exchangegenerator.service.ExchangeGeneratorServiceIntegrationTest.testExchangeGeneratorServiceGeneratesRates(ExchangeGeneratorServiceIntegrationTest.java:76)
+@TestPropertySource(properties = {
+        "spring.profiles.active=default"
+})
 public class ExchangeGeneratorServiceIntegrationTest {
-//
-//    @Autowired
-//    private EmbeddedKafkaBroker embeddedKafkaBroker;
-//
-//    @Autowired
-//    private ExchangeGeneratorService exchangeGeneratorService;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    private Consumer<String, ExchangeRequest> consumerForTest;
-//
-//    @BeforeEach
-//    void setUp() {
-//        assertNotNull(exchangeGeneratorService, "ExchangeGeneratorService should be autowired");
-//
-//        var valueDeserializer = new JsonDeserializer<>(ExchangeRequest.class, objectMapper);
-//        valueDeserializer.addTrustedPackages("*");
-//        valueDeserializer.setUseTypeHeaders(false);
-//
-//        var consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafkaBroker);
-//        consumerForTest = new DefaultKafkaConsumerFactory<>(
-//                consumerProps, new StringDeserializer(), valueDeserializer
-//        ).createConsumer();
-//
-//        consumerForTest.subscribe(List.of("exchange-rates")); // ваш топик
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        consumerForTest.close();
-//    }
-//
-//    @Test
-//    public void testExchangeGeneratorServiceGeneratesRates() {
-//        // when
-//        exchangeGeneratorService.generateAndUpdateRates();
-//
-//        // then
-//        var records = KafkaTestUtils.getRecords(consumerForTest, Duration.ofMillis(10000));
-//        assertThat(records.count()).isGreaterThanOrEqualTo(1);
-//
-//        var sentRequest = StreamSupport.stream(records.spliterator(), false)
-//                .filter(record -> "exchange-rates-update".equals(record.key()))
-//                .map(record -> record.value())
-//                .findFirst()
-//                .orElse(null);
-//
-//        assertNotNull(sentRequest);
-//        assertThat(sentRequest.getRates()).isNotEmpty();
-//
-//        for (ExchangeRateDto rate : sentRequest.getRates()) {
-//            assertThat(rate.getTitle()).isNotBlank();
-//            assertThat(rate.getName()).isNotBlank();
-//            assertThat(rate.getValue()).isGreaterThan(0);
-//        }
-//
-//        var currencyCodes = sentRequest.getRates().stream()
-//                .map(ExchangeRateDto::getName)
-//                .toList();
-//        assertThat(currencyCodes).contains("USD", "CNY");
-//    }
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private ExchangeGeneratorService exchangeGeneratorService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Consumer<String, ExchangeRequest> consumerForTest;
+
+    @BeforeEach
+    void setUp() {
+        assertNotNull(exchangeGeneratorService, "ExchangeGeneratorService should be autowired");
+
+        var valueDeserializer = new JsonDeserializer<>(ExchangeRequest.class, objectMapper);
+        valueDeserializer.addTrustedPackages("*");
+        valueDeserializer.setUseTypeHeaders(false);
+
+        var consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafkaBroker);
+        consumerForTest = new DefaultKafkaConsumerFactory<>(
+                consumerProps, new StringDeserializer(), valueDeserializer
+        ).createConsumer();
+
+        consumerForTest.subscribe(List.of("exchange-rates")); // ваш топик
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumerForTest.close();
+    }
+
+    @Test
+    public void testExchangeGeneratorServiceGeneratesRates() {
+        // when
+        exchangeGeneratorService.generateAndUpdateRates();
+
+        // then
+        var records = KafkaTestUtils.getRecords(consumerForTest, Duration.ofMillis(10000));
+        assertThat(records.count()).isGreaterThanOrEqualTo(1);
+
+        var sentRequest = StreamSupport.stream(records.spliterator(), false)
+                .filter(record -> "exchange-rates-update".equals(record.key()))
+                .map(record -> record.value())
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(sentRequest);
+        assertThat(sentRequest.getRates()).isNotEmpty();
+
+        for (ExchangeRateDto rate : sentRequest.getRates()) {
+            assertThat(rate.getTitle()).isNotBlank();
+            assertThat(rate.getName()).isNotBlank();
+            assertThat(rate.getValue()).isGreaterThan(0);
+        }
+
+        var currencyCodes = sentRequest.getRates().stream()
+                .map(ExchangeRateDto::getName)
+                .toList();
+        assertThat(currencyCodes).contains("USD", "CNY");
+    }
 }
